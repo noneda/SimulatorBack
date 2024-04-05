89f37ea7566c55b7e172a18e3ccbe6b5300d7b00
noneda <nonejda@gmail.com>
2024-04-04 19:00:40 -0500

    Backend:0.0.1 -> Utils - CalorEvap -> Se dan las Variables Y Ecuaciones

--- src/sql/Mysql.sql
+++ src/sql/Mysql.sql
@@ -15,25 +15,6 @@ CREATE TABLE DataObtained (
 );
 
 
-CREATE TABLE IF NOT EXISTS simulator_report (
-    simulator_report_id INT NOT NULL AUTO_INCREMENT,
-    total_mass VARCHAR(50),
-    user_id INT AUTO_INCREMENT NOT NULL,
-    humidity_percentage VARCHAR(50),
-    solids_percentage VARCHAR(50),
-    initial_solids VARCHAR(50),
-    specific_heat_trigo VARCHAR(50),
-    temperature_change VARCHAR(50),
-    latent_heat_evaporation_water VARCHAR(50),
-    initial_humidity VARCHAR(50),
-    total_wheat_mass_exit VARCHAR(50),
-    evaporated_water VARCHAR(50),
-    sensible_heat VARCHAR(50),
-    latent_heat VARCHAR(50),
-    total_heat VARCHAR(50),
-    PRIMARY KEY (simulator_report_id),
-    FOREIGN KEY (user_id) REFERENCES users (user_id) ON UPDATE CASCADE
-);
 
 CREATE TABLE ResultEquationsMachine (
     IdDataObtained INT PRIMARY KEY AUTO_INCREMENT,
@@ -57,4 +38,6 @@ CREATE TABLE Simulator (
     FOREIGN KEY (IdResults) REFERENCES ResultEquationsMachine(IdDataObtained),
     -- FOREIGN KEY (UserId) REFERENCES Users(UserId)
 );
+
+
 
--- src/utils/Simulator/CalorEvaporacion/index.js
+++ src/utils/Simulator/CalorEvaporacion/index.js
@@ -1,7 +1,49 @@
 /*
-    Here Craate Object for Creation about Evaporation and Energy
+    ! -> FUCK!!!!!!!!!!!!!!!!!!!!! HOW DO YOU THINK THAT I UNDERSTAND IT?
 */
+class EcuaEvaporacion{
 
-export const EcuaEvaporacion = {
+    constructor(InitialWeight, FinalWeight, heatUsed, massAEvaporate, heatIdealVaporization, supplyEnergy){
+        this.InitialWeight = InitialWeight
+        this.FinalWeight = FinalWeight
+        this.heatUsed = heatUsed
+        this.massAEvaporate = massAEvaporate
+        this.heatIdealVaporization = heatIdealVaporization
+        this.supplyEnergy = supplyEnergy
+    }
+
+    humidityPercentage() {
+        return (((InitialWeight - FinalWeight) / InitialWeight) * 100);
+    }
+
+    IdealEnergy(){
+
+        return (massAEvaporate * heatIdealVaporization)
+    
+    }
+    
+    EquationExcessEnergy(){
+    
+        return (heatUsed - IdealEnergy())
+    
+    }
+    
+    EquationDeficitEnergy(){
+    
+        return (IdealEnergy() - heatUsed)
+    
+    }
+    timeToEvaporateEverything(){
+/*
+    Esta funcion retornara el tiempo necesario para evaporar una cantidad de
+    de agua especifica usando la energia total para evaporar todo y dividirlo
+    por la cantidad de calor que se suministra cada segundo Vatios W o Joules por segundo
+*/
+    return (heatIdealVaporization / supplyEnergy)
+    }
+
 
 }
+
+
+export default EcuaEvaporacion;
